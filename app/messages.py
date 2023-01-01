145b1d19386a6683c215d1da0d4dd0bdcbd5d441
Vojislav Trujkic <voizlav@gmail.com>
2023-01-02 00:32:33 +0100

    Add basic functions to messages

---
+++ app/messages.py
@@ -0,0 +1,78 @@
+# TODO:
+
+def response_username_taken(username):
+  pass
+
+
+def response_username():
+  pass
+
+
+def response_password():
+  pass
+
+
+def response_code():
+  pass
+
+
+def response_size(size):
+  pass
+
+
+def response_filename():
+  pass
+
+
+def response_filename_len():
+  pass
+
+
+def response_snippet_created():
+  pass
+
+
+def response_snippet_edit():
+  pass
+
+
+def response_pass_user():
+  pass
+
+
+def response_hello(username):
+  pass
+
+
+def response_welcome():
+  pass
+
+
+def response_same_pass():
+  pass
+
+
+def response_login():
+  pass
+
+
+def response_register():
+  pass
+
+
+def response_delete_snippet():
+  pass
+
+
+def response_delete_title():
+  pass
+
+
+def response_not_found_header():
+  pass
+
+
+def response_not_found_paragraph():
+  pass
+
+
