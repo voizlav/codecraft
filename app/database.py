d991c8cacae3543c52289300e391716074b91afd
Vojislav Trujkic <voizlav@gmail.com>
2023-01-01 22:47:27 +0100

    Add snippet document class

--- app/database.py
+++ app/database.py
@@ -2,6 +2,17 @@ from mongoengine import *
 from datetime import *
 
 
+class Snippets(Document):
+  snippet_id    = StringField(required=True, primary_key=True)
+  user_id       = StringField(required=True)
+  username      = StringField(required=True, max_length=20)
+  source        = ListField(EmbeddedDocumentField(Source))
+  href          = StringField(required=True, max_length=10)
+  description   = StringField(reqiured=False)
+  views         = IntField(required=True, default=0)
+  deleted       = BooleanField(required=True, default=False)
+
+
 class Users(Document):
   user_id       = StringField(required=True, primary_key=True)
   username      = StringField(required=True, max_length=20)
