4ef38272ce1a1944e8887a151cd9999f85ae5afd
Vojislav Trujkic <voizlav@gmail.com>
2023-01-01 23:11:50 +0100

    Add basic error handlers

---
+++ app/error.py
@@ -0,0 +1,19 @@
+# TODO:
+
+from flask import render_template
+from app import app
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500.html"), 500
+
+
+@app.route("/raise_error")
+def raise_error():
+    raise Exception("This is a custom error")
