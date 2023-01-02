1be7c952166ac88f48014071384ce6012bed2c7d
Vojislav Trujkic <voizlav@gmail.com>
2023-01-02 12:46:59 +0100

    Add responses for registration

--- app/messages.py
+++ app/messages.py
@@ -1,78 +1,140 @@
 # TODO:
 
 def response_username_taken(username):
-  pass
+  return [
+    "Username is taken",
+    "Sorry, that username is already taken.",
+    "The username you have chosen is not available.",
+    "The username you entered is already in use.",
+    "We're sorry, but the username you have selected is unavailable.",
+    "Unfortunately, the username you entered is taken.",
+    "The username you have chosen is already registered.",
+    "We're sorry, but that username is not available.",
+    "The username you entered has already been taken by another user.",
+    "I'm sorry, but the username you selected is not available.",
+    "Unfortunately, the username you have chosen is already in use by another user.",
+    f"Sorry, but the username '{username}' is already taken. Please try a different one.",
+    f"I'm sorry, but the username '{username}' is not available. Please choose a different one.",
+    f"Unfortunately, the username '{username}' is not available. Please select a different one.",
+    f"The username '{username}' is already in use. Please choose a different one.",
+    f"I'm afraid the username '{username}' is not available. Please try another one.",
+    f"Unfortunately, the username '{username}' has already been taken. Please select a different one.",
+    f"The username '{username}' is unavailable. Please choose a different one.",
+    f"I'm sorry, but the username '{username}' is already in use. Please try a different one.",
+    f"The username '{username}' is not available. Please select a different one.",
+    f"Sorry, but the username '{username}' has already been taken. Please choose a different one."
+  ]
+
+
+def response_username_invalid():
+  return [
+    "Must provide valid username.",
+    "You must enter a valid username to register."
+    "Invalid username. Please enter a valid username to register."
+    "A valid username is required to complete registration."
+    "The username you entered is not valid. Please enter a different username to register."
+    "Please provide a valid username to complete registration."
+    "The username you entered is not valid. Please enter a different username."
+    "A valid username is required to create an account."
+    "Invalid username. Please enter a valid username to create an account."
+    "You must enter a valid username to create an account."
+    "The username you entered is not valid. Please enter a different username to create an account."
+  ]
 
 
 def response_username():
-  pass
+  return [
+    "Must provide username.",
+    "Please enter a username to continue registration.",
+    "Username is required for registration.",
+    "A username is necessary to create an account.",
+    "You must choose a username to complete registration.",
+    "A valid username is required to register.",
+    "Please enter a unique username to create your account.",
+    "Username field must be filled in to proceed with registration.",
+    "You need to select a username before you can complete the registration process.",
+    "A username is required in order to create your account.",
+    "Please provide a username to complete your registration."
+  ]
 
 
 def response_password():
-  pass
+  return [
+    "Must provide password",
+    "You must enter a password to create an account.",
+    "A password is required to complete registration.",
+    "Please provide a password to sign up.",
+    "You need to choose a password to finish registering.",
+    "A password must be entered to complete the registration process.",
+    "A password is mandatory for creating an account.",
+    "Please create a password in order to register.",
+    "You must select a password to complete the registration form.",
+    "A password is required to create your account.",
+    "Please enter a password to complete the sign up process."
+  ]
 
 
 def response_code():
-  pass
+  return []
 
 
 def response_size(size):
-  pass
+  return []
 
 
 def response_filename():
-  pass
+  return []
 
 
 def response_filename_len():
-  pass
+  return []
 
 
 def response_snippet_created():
-  pass
+  return []
 
 
 def response_snippet_edit():
-  pass
+  return []
 
 
 def response_pass_user():
-  pass
+  return []
 
 
 def response_hello(username):
-  pass
+  return []
 
 
 def response_welcome():
-  pass
+  return []
 
 
 def response_same_pass():
-  pass
+  return []
 
 
 def response_login():
-  pass
+  return []
 
 
 def response_register():
-  pass
+  return []
 
 
 def response_delete_snippet():
-  pass
+  return []
 
 
 def response_delete_title():
-  pass
+  return []
 
 
 def response_not_found_header():
-  pass
+  return []
 
 
 def response_not_found_paragraph():
-  pass
+  return []
 
 
