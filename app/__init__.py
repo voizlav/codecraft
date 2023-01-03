3e17afcf7f99092c53f891564d469ef93b41a0b5
Vojislav Trujkic <voizlav@gmail.com>
2023-01-03 20:26:58 +0100

    Add mapping for Jinja variables

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from flask import Flask
 from flask_session import Session
 from dotenv import load_dotenv
 from mongoengine import connect
-from app.utils import data_size, randomed_greet
+from app.utils import data_size, randomed
+from app import messages
 
 
 # Load the .env file and any exported environment variables
@@ -33,11 +34,22 @@ if not os.getenv("DATABASE_NAME"):
   raise RuntimeError("Database name is missing from the configuration")
 
 # Establish a connection to the MongoDB database using the mongoengine library
-connect(app.config["DATABASE_NAME"], app.config["DATABASE_URI"])
+connect(app.config["DATABASE_NAME"], host=app.config["DATABASE_URI"])
 
 # Customize Jinja filters for the app's template engine
 app.jinja_env.filters["hash"] = lambda source: hashlib.sha1(source.encode("utf-8")).hexdigest()
 app.jinja_env.filters["encode"] = lambda source: base64.urlsafe_b64encode(json.dumps(source).encode("utf-8")).decode("utf-8")
 app.jinja_env.filters["size"] = lambda source: round(len(source.encode()) / 1024, 2)
 app.jinja_env.filters["gzipped"] = lambda source: data_size(source)
-app.jinja_env.filters["randomed_greet"] = randomed_greet
+app.jinja_env.filters["randomed"] = randomed
+
+# Customize Jinja globals for the app's template engine
+app.jinja_env.globals["response_login"] = messages.response_login()
+app.jinja_env.globals["response_register"] = messages.response_register()
+app.jinja_env.globals["not_found_paragraph"] = messages.response_not_found_paragraph()
+app.jinja_env.globals["not_found_header"] = messages.response_not_found_header()
+app.jinja_env.globals["filename_too_long"] = messages.response_filename_len()
+app.jinja_env.globals["description_too_long"] = messages.response_description_len()
+
+
+from app import routes, error, database, utils
