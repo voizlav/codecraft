dac2a2875253652f62978e27d40a46f0f3fb557f
Vojislav Trujkic <voizlav@gmail.com>
2023-01-03 12:55:44 +0100

    Add accounts/register route

--- app/routes.py
+++ app/routes.py
@@ -36,3 +36,47 @@ def about():
 @app.route("/faq")
 def faq():
   return render_template("faq.html"), 200
+
+
+@app.route("/accounts/register", methods=["GET", "POST"])
+def register():
+  session.clear()
+
+  if request.method == "POST":
+
+    if not request.form.get("username"):
+      flash(f"{utils.randomed(messages.response_username())}", "warning")
+      return render_template("/accounts/register.html"), 403
+
+    elif not request.form.get("password"):
+      flash(f"{utils.randomed(messages.response_password())}", "warning")
+      return render_template("/accounts/register.html"), 403
+
+    if not request.form.get("confirmation"):
+      flash(f"{utils.randomed(messages.response_password())}", "warning")
+      return render_template("/accounts/register.html"), 403
+    
+    if not utils.name_scope(request.form.get("username")):
+      flash(f"{utils.randomed(messages.response_username_invalid())}", "warning")
+      return render_template("/accounts/register.html"), 403
+
+    if not utils.pass_scope(request.form.get("password"), request.form.get("confirmation")):
+      flash(f"{utils.randomed(messages.response_same_pass())}", "warning")
+      return render_template("/accounts/register.html"), 403
+
+    for user in Users.objects(username=request.form.get("username")):
+      if user.username == request.form.get("username"):
+        flash(f"{utils.randomed(messages.response_username_taken(user.username))}", "warning")
+        return render_template("/accounts/register.html"), 403
+
+    user_id = str(uuid.uuid4())
+    hashed_pass = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256:20000", salt_length=32)
+    create_user = Users(username=request.form.get("username"), password=hashed_pass, user_id=user_id)
+    create_user.save()
+    session["user_id"] = user_id
+
+    flash(f"{utils.randomed(messages.response_welcome('CodeCraft'))}", "success")
+    return render_template("index.html", username=create_user.username), 200
+  
+  else:
+    return render_template("/accounts/register.html"), 200
