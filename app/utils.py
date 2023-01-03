fc96d2155a7e979dd433bfadc86aac7048d94c47
Vojislav Trujkic <voizlav@gmail.com>
2023-01-03 20:24:57 +0100

    Add username validation feature

--- app/utils.py
+++ app/utils.py
@@ -10,3 +10,13 @@ def randomed(message):
 def pass_scope(password, comfirm):
     """ Check password input """
     return password == comfirm and len(password) > 0 and len(password) < 64
+
+
+def name_scope(username):
+    """ Check username input """
+    RESERVED = ["accounts", "new", "faq", "about"]
+
+    try:
+      return str(username).lower() not in RESERVED and len(username) <= 20 and len(username) > 0 and str(username).isalnum()     
+    except (AttributeError, TypeError):
+      return False
