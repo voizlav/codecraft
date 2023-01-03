e27f3b7552d8c6882ba96985b16ba34366525b17
Vojislav Trujkic <voizlav@gmail.com>
2023-01-03 20:23:30 +0100

    Add password validation function

--- app/utils.py
+++ app/utils.py
@@ -5,3 +5,8 @@ import random
 
 def randomed(message):
   return random.choice(message)
+
+
+def pass_scope(password, comfirm):
+    """ Check password input """
+    return password == comfirm and len(password) > 0 and len(password) < 64
