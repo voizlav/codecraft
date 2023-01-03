7ff4eff6257d40fd234991dac96bc4da8e58dc22
Vojislav Trujkic <voizlav@gmail.com>
2023-01-03 20:19:17 +0100

    Update the 404 error handler

--- app/error.py
+++ app/error.py
@@ -1,16 +1,12 @@
 # TODO:
 
-from flask import render_template
 from app import app
+from flask import render_template
 
 
 @app.errorhandler(404)
 def not_found_error(error):
-  return render_template(
-    "404.html",
-    paragraph=response_not_found_paragraph(),
-    header=response_not_found_header()
-  ), 404
+  return render_template("404.html"), 404
 
 
 @app.errorhandler(500)
