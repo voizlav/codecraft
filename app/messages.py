bb5692784350ebe7a745d697acd0978d4ece6aa7
Vojislav Trujkic <voizlav@gmail.com>
2023-01-04 12:20:33 +0100

    Add more various responses

--- app/messages.py
+++ app/messages.py
@@ -112,28 +112,93 @@ def response_same_pass():
 
 
 def response_code():
-  return []
+  return [
+    "It is not possible to submit an empty source code.",
+    "The source code cannot be left blank.",
+    "A source code cannot be submitted without any content.",
+    "It is required to have content in the source code.",
+    "An empty source code is not allowed.",
+    "The source code must contain something.",
+    "Looks like the source code is completely blank.",
+    "There's not a single line of code in the source.",
+    "The source code file is empty as a ghost town.",
+    "I think someone forgot to add any code to the source.",
+    "I don't see anything in the source code.",
+    "It's like the source code was never touched.",
+    "I'm not seeing any code in the source.",
+    "The source code looks like it's been erased.",
+    "I don't think there's anything in the source code.",
+    "The source code is as empty as a desert."
+  ]
 
 
 def response_size(size):
-  return []
-
-
-def response_filename():
-  return []
-
-
-def response_description_len():
   return [
-    "Your description is too long. Please keep it brief.",
-    "The description exceeds the maximum allowed length.",
-    "Please reduce the length of your description.",
-    "Description is too lengthy. Please shorten it.",
-    "The description is too long. Please revise it.",
-    "The description exceeds the maximum character limit.",
-    "Your description is too lengthy. Please trim it down.",
-    "The description is too long. Please make it shorter.",
-    "Please shorten the description as it exceeds the limit."
+    f"This source code is way too big at {size} KB - we need to trim it down.",
+    f"Whoa, {size} KB for code snippet? That's insane.",
+    f"I can't even begin to wrap my head around a source code this massive at {size} KB.",
+    f"Looks like we've got a giant source code on our hands at {size} KB.",
+    f"I don't think my server can handle this {size} KB source code.",
+    f"I'm not sure how we're going to manage this huge source code of {size} KB.",
+    f"Geez, {size} KB for source code? That's a lot to take in.",
+    f"I don't know if I'm ready for this massive {size} KB source code.",
+    f"This source code is seriously huge at {size} KB - where do we even begin?",
+    f"I think we might have bitten off more than we can handle with this {size} KB source code.",
+    f"Haha, I guess I went a little snippet-crazy at {size} KB!",
+    "Whoa, this source code is way too big for my liking.",
+    "I feel like I'm in the Matrix trying to navigate through this giant codebase.",
+    "I can't even wrap my head around this source code, it's so massive.",
+    "I don't have the patience to sift through all of this lengthy source code.",
+    "This source code is like a never-ending novel, it's so long.",
+    "I can't believe how massive this source code is. It's overwhelming.",
+    "I think I need a break after looking at this huge source code.",
+    "I'm starting to feel like I'll never reach the end of this giant source code.",
+    "I don't know if I'm up for tackling such a large source code.",
+    "I feel like I'm drowning in this massive source code."
+  ]
+
+
+def response_filename_blank():
+  return [
+    "You cannot leave the filename blank.",
+    "A filename must be entered.",
+    "The filename field cannot be left blank.",
+    "You must provide a filename.",
+    "Please enter a filename.",
+    "It is required to enter a filename.",
+    "An empty filename is not allowed.",
+    "The filename cannot be left empty.",
+    "A non-empty filename is required.",
+    "You must enter a valid filename.",
+    "Hey, you didn't give a name for the file.",
+    "What should I call this file? You didn't specify a name.",
+    "Oops, looks like you forgot to name the file.",
+    "I can't save it if you don't tell me what to name it.",
+    "A file needs a name, and you didn't provide one.",
+    "Don't forget to name the file before saving it.",
+    "You need to give this file a name before proceeding.",
+    "Uh oh, you didn't specify a file name.",
+    "You can't just leave the file name blank.",
+    "I need a name for this file - can you give me one?"
+  ]
+
+
+def response_filename_invalid():
+  return [
+    "Invalid file name.",
+    "File name is incorrect",
+    "File name does not meet the required standards.",
+    "Unacceptable file name.",
+    "File name does not follow the proper format.",
+    "Invalid characters in file name.",
+    "File name is not in the correct format.",
+    "Incorrect file name syntax.",
+    "File name does not comply with guidelines.",
+    "File name fails to meet the necessary criteria.",
+    "This file is like a bad joke - not valid.",
+    "I'm sorry, but this file is too sassy for its own good.",
+    "I think someone was feeling a bit lazy when they named this file.",
+    "This file is like a prank gone wrong - not valid."
   ]
 
 
@@ -155,16 +220,69 @@ def response_filename_len():
   ]
 
 
+def response_description_len():
+  return [
+    "Your description is too long. Please keep it brief.",
+    "The description exceeds the maximum allowed length.",
+    "Please reduce the length of your description.",
+    "Description is too lengthy. Please shorten it.",
+    "The description is too long. Please revise it.",
+    "The description exceeds the maximum character limit.",
+    "Your description is too lengthy. Please trim it down.",
+    "The description is too long. Please make it shorter.",
+    "Please shorten the description as it exceeds the limit."
+  ]
+
+
 def response_snippet_created():
-  return []
+  return [
+    "Looks like we're ready to rock and roll with this new code!",
+    "Code complete, party time!",
+    "Congratulations, we've brought a new life into the world: source code!",
+    "This code is a masterpiece, a work of art.",
+    "Looks like we've created the next big thing in software development.",
+    "Code creation success: let's pop some champagne!",
+    "We've created a code Frankenstein, and it's alive!",
+    "Code complete, and it's a thing of beauty.",
+    "We did it! A perfectly functioning code snippet has been conjured into existence.",
+    "The code snippet has been created and all is right in the world once again.",
+    "Another code snippet down, only a million more to go. #codinglife",
+    "Look out world, we've got another code snippet on the loose!",
+    "Another code snippet has been added to our arsenal of awesome code.",
+    "Code snippet creation success! Time for a well-deserved victory dance.",
+    "We've created another code snippet and it's a thing of beauty. #codingmagic"
+  ]
 
 
 def response_snippet_edit():
-  return []
+  return [
+    "Code fixin' done right!",
+    "We're now running on the latest and greatest codebase!",
+    "Source code upgrade complete, system running smoothly!",
+    "It's a code party in here!",
+    "Code update successful, time to celebrate with some bug-free programming!",
+    "Source code update complete, bring on the new features!",
+    "Code update success! Time to kick back and let the server do the work.",
+    "Code update complete, system running like a well-oiled machine!",
+    "The code gods are pleased with your updates and have bestowed upon you their blessing.",
+    "You have successfully avoided the code apocalypse and saved us all from certain doom.",
+    "Your code updates have brought peace to the world and cured world hunger."
+  ]
 
 
 def response_pass_user():
-  return []
+  return [
+    "Uh oh! It looks like your login information is not accurate.",
+    "Sorry, but the login details you provided are incorrect.",
+    "I'm afraid your username and password are not matching our records.",
+    "Unfortunately, the login information you entered is incorrect.",
+    "It appears that your login credentials are not correct.",
+    "Oof, it seems like the login information you provided is incorrect.",
+    "I'm sorry, but your login details do not match our records.",
+    "Oh no, it looks like your login information is incorrect.",
+    "Sorry, but the login credentials you entered are not correct.",
+    "I'm afraid the login details you provided are not accurate."
+  ]
 
 
 def response_hello(username):
@@ -246,11 +364,55 @@ def response_register():
   ]
 
 def response_delete_snippet():
-  return []
+  return [
+    "Are you sure you want to delete the snippet?",
+    "You sure you wanna delete that snippet?",
+    "Are you positive you want to get rid of the snippet?",
+    "Do you really want to erase the snippet?",
+    "You want to delete that snippet for sure?",
+    "You want to delete the snippet, right?",
+    "Are you certain you want to delete the snippet?",
+    "You sure you want to delete that snippet, right?",
+    "You definitely want to delete the snippet?",
+    "Are you completely sure you want to delete the snippet?",
+    "You want to delete the snippet without a doubt?",
+    "You really want to delete the snippet, right?",
+    "Are you sure you want to get rid of the snippet?",
+    "You definitely want to delete the snippet, right?",
+    "You want to delete the snippet for sure, right?",
+    "Are you certain you want to erase the snippet?",
+    "You sure you wanna nuke this thing?"
+  ]
 
 
 def response_delete_title():
-  return []
+  return [
+    "Get rid of that little piece of code",
+    "Wipe out that code segment",
+    "Trash that bit of code",
+    "Get rid of that code fragment",
+    "Eliminate that code snippet",
+    "Get rid of that code snippet",
+    "Remove that piece of code",
+    "Clear out that code snippet",
+    "Get rid of that code block",
+    "Delete that code snippet",
+    "Scrap that code segment",
+    "Get rid of that code segment",
+    "Remove that code fragment",
+    "Clear out that code fragment",
+    "Trash that code snippet",
+    "Get rid of that little bit of code",
+    "Take out that chunk of code",
+    "Ditch that piece of code",
+    "Chuck that code out",
+    "Cut that bit of code loose",
+    "Toss that piece of code",
+    "Trash that bit of code",
+    "Get rid of that code snippet",
+    "Dispose of that code",
+    "Bin that bit of code"
+  ]
 
 
 def response_not_found_header():
