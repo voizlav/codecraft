d93adf9d25c92707f148c505271a822a2e795c0e
Vojislav Trujkic <voizlav@gmail.com>
2023-01-05 23:48:43 +0100

    Small change in template rendering

--- app/error.py
+++ app/error.py
@@ -1,17 +1,15 @@
-# TODO:
-
 from app import app
 from flask import render_template
 
 
 @app.errorhandler(404)
 def not_found_error(error):
-  return render_template("404.html"), 404
+  return render_template("/error/404.jinja2"), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
-  return render_template("500.html"), 500
+  return render_template("/error/500.jinja2"), 500
 
 
 @app.route("/raise_error")
