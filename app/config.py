56b7c8d816ada4d18a84d6c0a40275b37fe2bb73
Vojislav Trujkic <voizlav@gmail.com>
2023-01-05 21:40:16 +0100

    Add a configuration file, with constants

---
+++ app/config.py
@@ -0,0 +1,37 @@
+import os
+
+
+DEBUG              = os.getenv("DEBUG")
+FLASK_DEBUG        = os.getenv("FLASK_DEBUG")
+
+SESSION_PERMANENT  = False
+SESSION_TYPE       = "filesystem"
+
+DATABASE_URI       = os.getenv("DATABASE_URI")
+DATABASE_NAME      = os.getenv("DATABASE_NAME")
+
+DATA_SIZE          = 2900
+HREF_LENGTH        = 10
+DESCRIPTION_LENGTH = 40
+FILENAME_LENGTH    = 20
+STATUS_LENGTH      = 30
+PASSWORD_LENGTH    = 120
+USERNAME_LENGTH    = 20
+
+RESERVED_USERNAMES = [
+  "admin",
+  "root",
+  "administrator",
+  "webmaster",
+  "support",
+  "info",
+  "contact",
+  "help",
+  "sysadmin",
+  "security",
+  "moderator",
+  "accounts",
+  "new",
+  "faq",
+  "about"
+]
