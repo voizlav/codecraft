9f347770ea9b1f7b4aadce1d20531eabf284febb
Vojislav Trujkic <voizlav@gmail.com>
2023-01-05 23:00:15 +0100

    Add route for username/snippet

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,15 @@
 from app import app
 from app import utils
 from app import messages
-from app.database import Users, Snippets, Source
+from database import Users, Snippets, Source
+from config import FILENAME_LENGTH, DESCRIPTION_LENGTH
 
-from flask import render_template, session, request, redirect, flash, abort, url_for, jsonify
-from werkzeug.security import check_password_hash, generate_password_hash
-
-import sys
 import uuid
 import hashlib
 from datetime import datetime
-from html import escape
+
+from flask import Response, render_template, session, request, redirect, flash, abort, url_for, jsonify, send_file
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 
@@ -28,19 +27,19 @@ def after_request(response):
 def index():
   try:
     user = Users.objects(user_id=session["user_id"]).first()
-    return render_template("index.html", username=user.username), 200
+    return render_template("index.jinja2", username=user.username), 200
   except (KeyError, AttributeError):
-    return render_template("/accounts/login.html"), 401
+    return render_template("/accounts/login.jinja2"), 401
 
 
 @app.route("/about")
 def about():
-  return render_template("/about.html"), 200
+  return render_template("/about.jinja2"), 200
 
 
 @app.route("/faq")
 def faq():
-  return render_template("/faq.html"), 200
+  return render_template("/faq.jinja2"), 200
 
 
 @app.route("/accounts/register", methods=["GET", "POST"])
@@ -51,32 +50,33 @@ def register():
 
     if not request.form.get("username"):
       flash(f"{utils.randomed(messages.response_username())}", "warning")
-      return render_template("/accounts/register.html"), 403
+      return render_template("/accounts/register.jinja2"), 403
 
     if not request.form.get("password"):
       flash(f"{utils.randomed(messages.response_password())}", "warning")
-      return render_template("/accounts/register.html"), 403
+      return render_template("/accounts/register.jinja2"), 403
 
     if not request.form.get("confirmation"):
       flash(f"{utils.randomed(messages.response_password())}", "warning")
-      return render_template("/accounts/register.html"), 403
+      return render_template("/accounts/register.jinja2"), 403
     
     if not utils.name_scope(request.form.get("username")):
       flash(f"{utils.randomed(messages.response_username_invalid())}", "warning")
-      return render_template("/accounts/register.html"), 403
+      return render_template("/accounts/register.jinja2"), 403
 
     if not utils.pass_scope(request.form.get("password"), request.form.get("confirmation")):
       flash(f"{utils.randomed(messages.response_same_pass())}", "warning")
-      return render_template("/accounts/register.html"), 403
+      return render_template("/accounts/register.jinja2"), 403
 
     for user in Users.objects(username=request.form.get("username")):
       if user.username == request.form.get("username"):
         flash(f"{utils.randomed(messages.response_username_taken(user.username))}", "warning")
-        return render_template("/accounts/register.html"), 403
+        return render_template("/accounts/register.jinja2"), 403
 
     user_id = str(uuid.uuid4())
     hashed_pass = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256:20000", salt_length=32)
-    create_user = Users(username=request.form.get("username"), password=hashed_pass, user_id=user_id)
+    user_status = utils.randomed(messages.response_set_default_status())
+    create_user = Users(username=request.form.get("username"), password=hashed_pass, user_id=user_id, status=user_status)
     create_user.save()
     session["user_id"] = user_id
 
@@ -84,7 +84,7 @@ def register():
     return redirect("/")
   
   else:
-    return render_template("/accounts/register.html"), 200
+    return render_template("/accounts/register.jinja2"), 200
 
 
 @app.route("/accounts/logout")
@@ -102,16 +102,16 @@ def login():
 
     if not request.form.get("username"):
       flash(f"{utils.randomed(messages.response_username())}", "warning")
-      return render_template("/accounts/login.html"), 403
+      return render_template("/accounts/login.jinja2"), 403
     
     if not request.form.get("password"):
       flash(f"{utils.randomed(messages.response_password())}", "warning")
-      return render_template("/accounts/login.html"), 403
+      return render_template("/accounts/login.jinja2"), 403
         
     for user in Users.objects(username=request.form.get("username")):
       if not check_password_hash(user.password, request.form.get("password")):
         flash(utils.randomed(messages.response_pass_user()), "warning")
-        return render_template("/accounts/login.html"), 403
+        return render_template("/accounts/login.jinja2"), 403
       else:
         session["user_id"] = user.user_id
         user.update(set__last_online=datetime.utcnow)
@@ -120,10 +120,10 @@ def login():
         return redirect("/")
     
     flash(utils.randomed(messages.response_pass_user()), "warning")
-    return render_template("/accounts/login.html"), 403
+    return render_template("/accounts/login.jinja2"), 403
 
   else:
-    return render_template("/accounts/login.html"), 200
+    return render_template("/accounts/login.jinja2"), 200
 
 
 @app.route("/<username>")
@@ -131,7 +131,7 @@ def username(username):
 
   for user in Users.objects(username=username):
     snippets = Snippets.objects(username=username)
-    return render_template("/user.html", user=user, snippets=snippets)
+    return render_template("/username/profile.jinja2", user=user, snippets=snippets)
 
   abort(404)
 
@@ -151,16 +151,16 @@ def new():
       return redirect("/")
 
     if not request.form.get("filename"):
-      flash(utils.randomed(messages.response_filename()), "warning")
+      flash(utils.randomed(messages.response_filename_blank()), "warning")
       return redirect("/")
     
-    if len(request.form.get("filename")) > 20:
+    if len(request.form.get("filename")) > FILENAME_LENGTH:
       flash(utils.randomed(messages.response_filename_len()), "warning")
       return redirect("/")
     
     if request.form.get("description"):
-      description = escape(request.form.get("description"))
-      if description > 40:
+      description = request.form.get("description")
+      if len(description) > DESCRIPTION_LENGTH:
         flash(utils.randomed(messages.response_description_len(), "warning"))
         return redirect("/")
     else:
@@ -190,3 +190,18 @@ def new():
     
   else:
     abort(404)
+
+
+@app.route("/<username>/<snippet>")
+def snippet(username, snippet):
+
+  for snippet in Snippets.objects(username=username, href=snippet):
+    user = Users.objects(username=username).first()
+    snippet.views += 1
+    snippet.save()
+
+    return render_template("/username/snippet.jinja2", username=username, snippet=snippet, sourcecode=snippet.source[-1], user_id=user.user_id)
+
+  abort(404)
+
+
